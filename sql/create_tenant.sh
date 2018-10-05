101a064884c12f72e4e903038fc06684e3325af0
Tarun Lalwani <tarunlalwani@gmail.com>
2018-10-05 10:30:43 +0530

    New process tenant scripts

--- sql/create_tenant.sh
+++ sql/create_tenant.sh
@@ -2,7 +2,8 @@
 
 export tenant_id=$1
 
-if [ ! -f "TenantGoLive.$tenant_id.sql" ]; then
-  cat TenantGoLive.template.sql | sed -e "s|__tenant_id|$tenant_id|g" > TenantGoLive.$tenant_id.sql
-fi
+#if [ ! -f "TenantGoLive.$tenant_id.sql" ]; then
+#  cat TenantGoLive.template.sql | sed -e "s|__tenant_id|$tenant_id|g" > TenantGoLive.$tenant_id.sql
+#fi
 
+cat templates/billingslab_template.sql |  sed -e "s|__tenant_id|$tenant_id|g" >> billingslab.sql
