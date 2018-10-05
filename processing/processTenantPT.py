cf730c91676fb5ab241e3f3dce01e9db6897d236
Tarun Lalwani <tarunlalwani@gmail.com>
2018-10-05 16:38:06 +0530

    Fixed path issues

--- processing/processTenantPT.py
+++ processing/processTenantPT.py
@@ -1,6 +1,6 @@
 from inspect import getsourcefile
 import os.path as path, sys
-current_dir = path.dirname(path.dirname(path.abspath(getsourcefile(lambda:0))))
+current_dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
 sys.path.insert(0, current_dir[:current_dir.rfind(path.sep)])
 
 import traceback
