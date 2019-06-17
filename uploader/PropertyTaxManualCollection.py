f88cbc6461f1ca70880500c7b1315ad98325e2ff
Tarun Lalwani <tarunlalwani@gmail.com>
2019-06-17 17:48:47 +0530

    Add updated scripts

--- uploader/PropertyTaxManualCollection.py
+++ uploader/PropertyTaxManualCollection.py
@@ -6,8 +6,8 @@ from common import search_receipt, search_property, search_demand, update_demand
     update_property, superuser_login
 
 
-def create_manual_receipt_collection(auth_token, tenant_id, property_id, assessment_year, paid_by, payment_amount
-                                     , payment_type="Cash"):
+def create_manual_receipt_collection(auth_token, tenant_id, property_id, assessment_year, paid_by, payment_amount_map: dict
+                                     , g8_receiptnumber, g8_receiptdate, total_amount_paid, payment_type="Cash"):
     properties = search_property(auth_token, tenant_id, property_id)
     properties = properties["Properties"]
 
@@ -36,14 +36,14 @@ def create_manual_receipt_collection(auth_token, tenant_id, property_id, assessm
         new_assessment_property[0]["propertyDetails"][0]["financialYear"] = assessment_year
         new_property = update_property(auth_token, tenant_id, new_assessment_property)
         return create_manual_receipt_collection(auth_token, tenant_id, property_id, assessment_year, paid_by,
-                                                payment_amount)
+                                                payment_amount_map, g8_receiptnumber, g8_receiptdate, total_amount_paid, payment_type)
 
     demand = search_demand(auth_token, tenant_id, latest_consumer_code, "PT")["Demands"]
     demand_id = demand[0]["id"]
 
     for demand_detail in demand[0]["demandDetails"]:
-        if demand_detail["taxHeadMasterCode"] == "PT_TAX":
-            demand_detail["taxAmount"] = payment_amount
+        if demand_detail["taxHeadMasterCode"] in payment_amount_map:
+            demand_detail["taxAmount"] = payment_amount_map[demand_detail["taxHeadMasterCode"]]
         else:
             demand_detail["taxAmount"] = 0
 
@@ -53,27 +53,60 @@ def create_manual_receipt_collection(auth_token, tenant_id, property_id, assessm
 
     bill_copy = copy.deepcopy(bill)
 
+    amount_paid = bill[0]["taxAndPayments"][0]["taxAmount"]
+
+    if amount_paid != total_amount_paid:
+        raise Exception("Total paid calculated and actual is different. Calculated={} and Actual={}".format(amount_paid, total_amount_paid))
+
     receipt_request = [{
         "Bill": bill_copy,
         "instrument": {
+            "transactionNumber": None,
+            "transactionDate": None, # in Date format DD-MM-YYYY
+            "transactionDateInput": "",
+            "payee": paid_by,
+            "ifscCode": "",
             "instrumentType": {
                 "name": payment_type
             },
             "tenantId": tenant_id,
-            "amount": payment_amount
+            "amount": amount_paid
         },
         "tenantId": tenant_id
     }]
 
     bill_copy[0]["paidBy"] = paid_by
-    bill_copy[0]["billDetails"][0]["collectionType"] = "COUNTER"
-    bill_copy[0]["taxAndPayments"][0]["amountPaid"] = payment_amount
+    bd = bill_copy[0]["billDetails"][0]
+    bd["collectionType"] = "COUNTER"
+    bd["manualReceiptNumber"] = g8_receiptnumber
+    bd["manualReceiptDate"] = g8_receiptdate
+    bill_copy[0]["taxAndPayments"][0]["amountPaid"] = amount_paid
 
     receipt_response = create_receipt(auth_token, tenant_id, receipt_request)
     return receipt_response
 
 
+amounts = {
+    "PT_ADHOC_REBATE": 0,
+    "PT_ADVANCE_CARRYFORWARD": 0,
+    "PT_OWNER_EXEMPTION": 0,
+    "PT_TIME_REBATE": 225,
+    "PT_UNIT_USAGE_EXEMPTION": 0,
+    "PT_ADHOC_PENALTY": 0,
+    "PT_TAX": 2250,
+    "PT_FIRE_CESS": 113,
+    "PT_CANCER_CESS": 0,
+    "PT_TIME_PENALTY": 0,
+    "PT_TIME_INTEREST": 0
+}
+
+
+def to_epoch(date_time):
+    import time
+    return int(time.mktime(time.strptime(date_time, "%d/%m/%y")))
+
+
 if __name__ == "__main__":
     auth_token = superuser_login()["access_token"]
-    create_manual_receipt_collection(auth_token, "pb.testing", "PT-1012-072874", "2014-15", "Tarun", 2000)
-tl
+    print(.o(auth_token, "pb.testing", "PT-1012-072874", "2017-18", "Tarun", amounts,"20992/43",
+                                     to_epoch("06/04/17"), 2138, "Cash"))
